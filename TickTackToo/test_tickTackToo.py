fd064a3e2b7eb05cc546e97369a0b1f63cccd10d
krzjanas <krzjanas@git.com>
2015-11-30 11:35:59 +0100

    TickTackToo

---
+++ TickTackToo/test_tickTackToo.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+
+class TestTickTackToo(TestCase):
+  def test_clear(self):
+    self.fail()
+
+  def test_printStage(self):
+    self.fail()
+
+  def test_setX(self):
+    self.fail()
+
+  def test_setO(self):
+    self.fail()
+
+  def test_checkWin(self):
+    self.fail()
+
+  def test_setWinLine(self):
+    self.fail()
---
+++ TickTackToo/test_tickTackTooStage.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+
+class TestTickTackTooStage(TestCase):
+  def test_clear(self):
+    self.fail()
+
+  def test_printStage(self):
+    self.fail()
+
+  def test_setWinLine(self):
+    self.fail()
+
+  def test_setX(self):
+    self.fail()
+
+  def test_setO(self):
+    self.fail()
+
+  def test_setStage(self):
+    self.fail()
